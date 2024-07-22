d74356707ff4b24a0cabcc08cc805b4ad83435fb
shiquan35 <ongshiquan35@gmail.com>
2024-07-22 22:25:24 +0800

    updated background

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Container from "./(container)/container";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
       <Container />
     </div>
   );
